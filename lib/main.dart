d7deb981197b9602483394892bc8c6d5c63eead6
brenoos <breno.oliver07@gmail.com>
2020-03-26 21:15:20 -0300

    add dismissible

--- lib/main.dart
+++ lib/main.dart
@@ -83,26 +83,44 @@ class _HomeState extends State<Home> {
             child: ListView.builder(
               padding: EdgeInsets.only(top: 10.0),
               itemCount: _toDoList.length,
-              itemBuilder: (context, index) {
-                return CheckboxListTile(
-                  title: Text(_toDoList[index]["title"]),
-                  value: _toDoList[index]["ok"],
-                  secondary: CircleAvatar(
-                    child: Icon(
-                      _toDoList[index]["ok"] ? Icons.check : Icons.error,
-                    ),
-                  ),
-                  onChanged: (checked) {
-                    setState(() {
-                      _toDoList[index]["ok"] = checked;
-                      _saveData();
-                    });
-                  },
-                );
-              },
+              itemBuilder: buildItem,
             ),
           )
         ],
+      ),
+    );
+  }
+
+  Widget buildItem(context, index) {
+    return Dismissible(
+      key: Key(
+        DateTime.now().millisecondsSinceEpoch.toString(),
+      ),
+      background: Container(
+        color: Colors.red,
+        child: Align(
+          alignment: Alignment(-0.9, 0.0),
+          child: Icon(
+            Icons.delete,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      direction: DismissDirection.startToEnd,
+      child: CheckboxListTile(
+        title: Text(_toDoList[index]["title"]),
+        value: _toDoList[index]["ok"],
+        secondary: CircleAvatar(
+          child: Icon(
+            _toDoList[index]["ok"] ? Icons.check : Icons.error,
+          ),
+        ),
+        onChanged: (checked) {
+          setState(() {
+            _toDoList[index]["ok"] = checked;
+            _saveData();
+          });
+        },
       ),
     );
   }
